371abef8363e1ab33cfa8a8c09fa39fa99797f48
randy steward <rsteward117@gmail.com>
2020-11-09 19:44:37 -0600

    commit files

---
+++ Library.js
@@ -0,0 +1,9 @@
+
+var myDiv = document.querySelector(".formlayout");
+myDiv.style.display = "none";
+
+function showDiv(){
+    if(myDiv.style.display === "none"){
+        myDiv.style.display = "block";
+    }
+}
