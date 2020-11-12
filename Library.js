f8ce80f9d8e2a6e553ca189072b3e191e73f9953
rsteward117 <66583624+rsteward117@users.noreply.github.com>
2020-11-12 01:52:08 -0600

    Delete Library.js

--- Library.js
+++
@@ -1,9 +0,0 @@
-
-var myDiv = document.querySelector(".formlayout");
-myDiv.style.display = "none";
-
-function showDiv(){
-    if(myDiv.style.display === "none"){
-        myDiv.style.display = "block";
-    }
-}
